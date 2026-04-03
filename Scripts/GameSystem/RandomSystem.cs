2f2bfc8d92a53e4d1e5d584f99fee641a354c56a
DESKTOP-A2MHDHE\khjj0 <khjj07@gmail.com>
2026-04-03 22:37:03 +0900

    faet : 랜덤 카운터 추가

--- Scripts/GameSystem/RandomSystem.cs
+++ Scripts/GameSystem/RandomSystem.cs
@@ -8,12 +8,17 @@ namespace KCoreKit
 {
     public static class RandomSystem
     {
-        private static Random random;
+        private static int _counter;
+        private static int _seed;
+        private static Random _random;
 
         public static void SetSeed(int seed = 0)
         {
-            random = new Random(seed);
+            _counter = 0;
+            _seed = seed;
+            _random = new Random(_seed);
         }
+        
         
         public static T GetRandomElement<T>(this IList<T> array)
         {
@@ -21,7 +26,7 @@ namespace KCoreKit
             {
                 return default;
             }
-            return array.OrderBy(x => random.Next()).First();
+            return array.OrderBy(x => Next()).First();
         }
         public static List<T> GetRandomElements<T>(this IList<T> array, int number)
         {
@@ -36,26 +41,48 @@ namespace KCoreKit
 
             // 3. 효율적인 추출 (Fisher-Yates 셔플 응용 또는 LINQ 활용)
             // 소규모라면 OrderBy도 괜찮지만, 아래 방식이 가독성과 범용성이 좋습니다.
-            return array.OrderBy(x => random.Next())
+            return array.OrderBy(x => Next())
                 .Take(count)
                 .ToList();
         }
         public static float Range(float min, float max)
         {
-            return (float)(random.NextDouble() * (max - min) + min);
+            return Next() * (max - min) + min;
         }
         
         public static int RangeInt(int min, int max)
         {
-            return random.Next(min, max);
+            return Next(min, max);
         }
 
         public static Vector3 RangeVector3(Vector3 min, Vector3 max)
         {
-            var x = (float)(random.NextDouble() * (max.x - min.x) + min.x);
-            var y = (float)(random.NextDouble() * (max.y - min.y) + min.y);
-            var z = (float)(random.NextDouble() * (max.z - min.z) + min.z);
+            var x = Next() * (max.x - min.x) + min.x;
+            var y = Next() * (max.y - min.y) + min.y;
+            var z = Next() * (max.z - min.z) + min.z;
             return new Vector3(x, y, z);
+        }
+
+        public static float Next()
+        {
+            _counter++;
+            return _random.Next();
+        }
+
+        public static int Next(int min, int max)
+        {
+            _counter++;
+            return _random.Next(min, max);
+        }
+        
+        public static int GetSeed()
+        {
+            return _seed;
+        }
+
+        public static int GetCounter()
+        {
+            return _counter;
         }
     }
 }
