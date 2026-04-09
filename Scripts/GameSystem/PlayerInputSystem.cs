baa4f955b34ebb04d4e7761cb17c4b03baed95e3
khjj07@gmail.com <khjj07@gmail.com>
2026-04-09 09:58:59 +0900

    feat : PlayerInputSystem추가

---
+++ Scripts/GameSystem/PlayerInputSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using KCoreKit;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Streams
+{
+    public enum PlayerActionType
+    {
+        Started,
+        Performed,
+        Canceled
+    }
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerInputSystem : GameSubSystemBase
+    {
+        private PlayerInput _playerInput;
+
+        private void Awake()
+        {
+            _playerInput = GetComponentInChildren<PlayerInput>();
+        }
+
+        public void RegisterAction(Action<InputAction.CallbackContext> callback)
+        {
+            _playerInput.onActionTriggered += callback;
+        }
+        
+        public void RegisterAction(string action, PlayerActionType actionType,
+            Action<InputAction.CallbackContext> callback)
+        {
+            switch (actionType)
+            {
+                case PlayerActionType.Started:
+                    _playerInput.actions[action].started += callback;
+                    break;
+                case PlayerActionType.Performed:
+                    _playerInput.actions[action].performed += callback;
+                    break;
+                case PlayerActionType.Canceled:
+                    _playerInput.actions[action].canceled += callback;
+                    break;
+            }
+        }
+    }
+
+
+}
